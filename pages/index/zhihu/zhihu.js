19db920bb4f276a92ebf02d2f28735aa5e2a0a5f
yuanwy <yuan13675013>
2019-01-24 00:30:27 +0800

    知乎

--- pages/index/zhihu/zhihu.js
+++ pages/index/zhihu/zhihu.js
@@ -7,41 +7,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topData: {
-      date: "20140523",
-      stories: [
-        {
-          title: "中国古代家具发展到今天有两个高峰，一个两宋一个明末（多图）",
-          ga_prefix: "052321",
-          images: [
-            "http://p1.zhimg.com/45/b9/45b9f057fc1957ed2c946814342c0f02.jpg"
-          ],
-          type: 0,
-          id: 3930445
-        },
-      ],
-      top_stories: [
-        {
-          title: "商场和很多人家里，竹制家具越来越多（多图）",
-          image: "http://p2.zhimg.com/9a/15/9a1570bb9e5fa53ae9fb9269a56ee019.jpg",
-          ga_prefix: "052315",
-          type: 0,
-          id: 3930883
-        }
-      ]
-    }
+    topData: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    wx.request({
-      method :"GET",
-      url: 'https://news-at.zhihu.com/api/4/news/latest',
-      success(res) {
-        this.topData = res.data;
-      }
+    this.getNewDatas();
+    wx.showLoading({
+      title: 'Loading...',
     })
   },
 
@@ -77,7 +52,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    this.getNewDatas();
   },
 
   /**
@@ -92,5 +67,30 @@ Page({
    */
   onShareAppMessage: function() {
 
+  },
+  
+  /**
+   * 请求首页最新消息
+   */
+  getNewDatas: function() {
+    var self = this;
+    wx.request({
+      url: 'https://news-at.zhihu.com/api/4/news/latest',
+      success(res) {
+        console.log(res.data);
+        self.setData({
+          topData: res.data
+        })
+      },
+      complete() {
+        wx.hideLoading();
+        wx.stopPullDownRefresh();
+      },
+      fail() {
+        wx.showToast({
+          title: '加载失败',
+        })
+      }
+    })
   }
 })
